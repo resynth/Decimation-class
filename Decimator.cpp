aaa19377321d1810216ea6da5ad4aedaaa0e0911
resynth <63058108+resynth@users.noreply.github.com>
2021-02-25 03:28:16 +0000

    Add files via upload
    0 to 10000Hz, 3dB ripple.
    11000 to 22050, -96dB

--- Decimator.cpp
+++ Decimator.cpp
@@ -1,13 +1,17 @@
 #include "Decimator.h"
 #include <string.h>
 
+
+
+
 int Decimator::setup(unsigned int factor, unsigned int blockSize)
 {
 	cleanup();
 	this->blockSize = blockSize;
 	this->factor = factor;
-	this->numTaps = 9;
+	//this->numTaps = 115;
 	
+	output.resize(blockSize * 0.5);
 	source = (ne10_float32_t*) NE10_MALLOC (blockSize * sizeof (ne10_float32_t));
 	destiny = (ne10_float32_t*) NE10_MALLOC ((blockSize*0.5) * sizeof (ne10_float32_t));
 	if(!destiny || !source)
@@ -16,15 +20,15 @@ int Decimator::setup(unsigned int factor, unsigned int blockSize)
 		return -1;
 	}
 	
-	firFilterCoeff = (ne10_float32_t *) NE10_MALLOC (this->numTaps * sizeof (ne10_float32_t));
+	//firFilterCoeff = (ne10_float32_t *) NE10_MALLOC (this->numTaps * sizeof (ne10_float32_t));
 	firFilterState = (ne10_float32_t *) NE10_MALLOC ((this->numTaps + blockSize - 1) * sizeof (ne10_float32_t));
-	if(!firFilterState || !firFilterCoeff)
+	if(!firFilterState)// || !firFilterCoeff)
 		return -1;
-		
+/*		
 	firFilterCoeff[0] = 0.009955733596956272; firFilterCoeff[1] = 0.04252038304633031; firFilterCoeff[2] = 0.09926778090651416; 
 	firFilterCoeff[3] = 0.1575504280232556;   firFilterCoeff[4] = 0.18263400694813212; firFilterCoeff[5] = 0.1575504280232556; 
 	firFilterCoeff[6] = 0.09926778090651416;  firFilterCoeff[7] = 0.04252038304633031; firFilterCoeff[8] = 0.009955733596956272; 
-
+*/
 	ne10_fir_decimate_init_float (	
 		&firFilter,
 		this->numTaps,
@@ -42,7 +46,7 @@ std::vector<float>& Decimator::decimate(std::vector<float>& input)
 {
 	memcpy(source, input.data(), sizeof(input[0]) * blockSize);
 	ne10_fir_decimate_float_neon(&firFilter, source, destiny, blockSize); //const ne10_fir_decimate_instance_f32_t * , ne10_float32_t * pSrc, ne10_float32_t * pDst, ne10_uint32_t 	blockSize
-	memcpy(output.data(), destiny, sizeof(destiny[0]) * blockSize);
+	memcpy(output.data(), destiny, sizeof(destiny[0]) * (blockSize * 0.5));
 	
 	return output; 
 }
--- render.cpp
+++ render.cpp
@@ -1,12 +1,42 @@
 #include <Bela.h>
 
+#include "Decimator.h"
+#include <vector>
+
+Decimator decimator;
+std::vector<float> inBuf;
+std::vector<float> outBuf;
+
 bool setup(BelaContext *context, void *userData)
 {
+	decimator.setup(2, context->audioFrames);
+	inBuf.resize(context->audioFrames);
+	outBuf.resize(context->audioFrames * 0.5);
+	
 	return true;
 }
 
 void render(BelaContext *context, void *userData)
 {
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		inBuf[n] = audioRead(context, n, 0);
+	}
+	outBuf = decimator.decimate(inBuf);
+	
+	
+	// dirty test
+	unsigned int flip = 0; 
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		if(flip == 0) {
+			flip = 1;
+			audioWrite(context, n, 0, outBuf[n]);	
+		}
+		else {
+			flip = 0;
+			audioWrite(context, n, 0, outBuf[n-1]);
+		}
+		
+	}
 
 }
 
